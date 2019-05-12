421cd73f50c90e9f0a3e67682a9866ab4e277a3f
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-05-11 18:45:07 -0700

    adding hack for github pages url

--- js/nav-links.js
+++ js/nav-links.js
@@ -1,12 +1,14 @@
 function activateNavLinks() {
     
     let url = window.location.pathname;
-    console.log("."+url);
+    let new_url ="./"+url.split("/").pop(); //hack to make this work with github pages url
+    console.log(new_url);
+
     let nav_links = document.querySelectorAll("#menu li a")
     nav_links.forEach (function(nav_link){
         let link = nav_link.getAttribute("href");
         console.log(link); 
-        if("."+url==link){
+        if(new_url==link){
             document.querySelector(".active").classList.remove("active");
             nav_link.classList.add("active");
         }
