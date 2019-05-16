1d39f6bbd9cabb89bd59d18eafe273b682b78c2d
Nadya Ilinskaya <nadya.ilinskiy@gmail.com>
2019-05-15 22:25:48 -0700

    refactor welcome.js, adding domload.js

---
+++ js/domload.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    greetings();
+    welcome();
+    activateNavLinks();
+    activateGallery();
+  });
--- js/welcome.js
+++ js/welcome.js
@@ -3,11 +3,44 @@ function welcome() {
     let guestName = document.querySelector("#guest_name");
     let submitButton = document.querySelector("#submit_btn");
    
-    // storing input from input-form
+    
+    let form = document.querySelector("form"); 
+    //if form exist on the page
+    if(form){ 
+    //listen for click event on submit button
     submitButton.addEventListener ("click", function() {
-        localStorage.setItem('guest_name', guestName.value);
-    });
-
-    let storedName =  localStorage.getItem('guest_name');
-    console.log(storedName);
+        // storing input from input-form
+    localStorage.setItem('guest_name', guestName.value);
+        greetings();
+    // let storedName =  localStorage.getItem('guest_name');
+    // let greetings = document.querySelector("#greetings");
+    //     if (storedName) {
+    //         greetings.innerHTML = "Welcome " + storedName;
+    //         event.preventDefault();   
+    //         } 
+    //     else {
+    //         greetings.innerHTML = "Please enter your name";
+    //         event.preventDefault();
+    //         }
+        });
+    }
 }
+
+function greetings(){
+    let storedName =  localStorage.getItem('guest_name');
+    let greetings = document.querySelector("#greetings");
+    console.log(storedName);
+    if (storedName) {
+        greetings.innerHTML = "Welcome " + storedName;
+      //  event.preventDefault();   
+        } 
+    else {
+        greetings.innerHTML = "Please enter your name";
+        //event.preventDefault();
+        }
+}
+
+// function hideForm(){
+//     formDiv = document.getElementById('form');
+//     formDiv.style.display = 'none';
+// }
